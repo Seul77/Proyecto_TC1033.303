03dd239c6e89e9eba6c53eb2edd6a15a4acbc6eb
Seul77 <a01714080@tec.mx>
2025-12-07 17:56:39 -0600

    Refactor Invernadero.cpp to improve structure

--- Invernadero.cpp
+++ Invernadero.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+
+// Se incluyen los archivos de cada tipo de planta e información
 #include "CarnivoraInfo.h"
 #include "CarnivoraCuidados.h"
 #include "CactusInfo.h"
@@ -7,38 +9,50 @@ using namespace std;
 #include "FlorInfo.h"
 #include "FlorCuidados.h"
 #include "Planta.h"
+
+// Funcion main
 int main() {
     
     int tipoPlanta = 0;
+
+    // Se crean plantas carnívoras con sus datos de información
     Carnivora plantaCarnivora1("Venus atrapamoscas", "Dionaea muscipula", 3, false, 299.99, "Trampa de mandíbula");
     Carnivora plantaCarnivora2("Rocio del sol", "Drosera capensis", 2, false, 150.00, "Trampa pegajosa");
     Carnivora plantaCarnivora3("Planta jarro", "Nepenthes alata", 4, false, 450.50, "Trampa de jarra");
+
+    // Se crean los cuidados correspondientes
     CarnivoraCuidados cuidadosCarnivora1(3, "Sustrato pobre en nutrientes", 22.5, "Pequeño", 5);
     CarnivoraCuidados cuidadosCarnivora2(4, "Musgo sphagnum", 24.0, "Mediano", 7);
     CarnivoraCuidados cuidadosCarnivora3(2, "Turba y arena", 20.0, "Grande", 4);
 
+    // Lo mismo para Cactus 
     CactusInfo plantaCactus1("Barril de oro", "Echinocactus grusonii", 5, false, 99.99, 12.0);
     CactusInfo plantaCactus2("Nopal", "Opuntia ficus-indica", 3, false, 49.99, 8.0);
     CactusInfo plantaCactus3("Biznaga elongada", "Mammillaria elongata", 4, false, 120.00, 10.0);
+    
     CactusCuidados cuidadosCactus1(1, "Tierra arenosa", 28.0, 8.0, false);
     CactusCuidados cuidadosCactus2(2, "Mezcla de tierra para cactus", 26.0, 6.0, true);
     CactusCuidados cuidadosCactus3(1, "Sustrato bien drenado", 30.0, 7.0, false);
 
+    // Y para flores
     FlorInfo plantaFlor1("Rosa", "Rosa spp.", 1, false, 10.00, 7.0, 5.0, "Rojo");
     FlorInfo plantaFlor2("Tulipán", "Tulipa gesneriana", 1, false, 8.50, 10.0, 4.0, "Amarillo");
     FlorInfo plantaFlor3("Girasol", "Helianthus annuus", 1, false, 12.00, 15.0, 10.0, "Amarillo");
     FlorInfo plantaFlor4("Orquídea", "Orchidaceae spp.", 2, false, 20.00, 30.0, 6.0, "Violeta");
+    
     FlorCuidados cuidadosFlor1(7, "Tierra rica en nutrientes", 20.0, "Alto", true, true);
     FlorCuidados cuidadosFlor2(5, "Sustrato bien drenado", 18.0, "Medio", false, true);
     FlorCuidados cuidadosFlor3(6, "Tierra fértil", 22.0, "Alto", true, false);
     FlorCuidados cuidadosFlor4(4, "Mezcla para orquídeas", 24.0, "Bajo", true, false);
     
+    // Menú principal: 
     cout << "Bienvenido al sistema de gestión de invernadero, ¿qué planta desea consultar? (ingrese el numero)\n"
         << "1. Planta carnívora\n"
         << "2. Cactus\n"
         << "3. Flor\n";
     cin >> tipoPlanta;
 
+    // Opción si se elige planta carnívora:
     if (tipoPlanta == 1) {
         cout << "\nHa seleccionado la planta carnívora.\n"
         << "¿Cual planta carnívora desea seleccionar?\n"
@@ -47,6 +61,8 @@ int main() {
         << "3. " << plantaCarnivora3.getNombre() << "\n";
         int plantaSeleccionada;
         cin >> plantaSeleccionada;
+
+        // Se muestra la planta elegida
         cout << "La planta seleccionada fue: \n";
         if (plantaSeleccionada == 1) { 
             cout << plantaCarnivora1.getNombre() << endl;
@@ -61,12 +77,16 @@ int main() {
             cout << "Opción no válida.\n";
             return 0;
         }
+
+        // Se le pregunta al usuario que desea hacer siguiente
             cout << "¿Qué información desea ver?\n"
                  << "1. Información de la planta\n"
                  << "2. Cuidados de la planta\n"
                  << "3. Hacer un pedido\n";
             int infoSeleccionada;
             cin >> infoSeleccionada;
+
+            // Se muestra la información, precios, o cuidados correspondientes
             if (infoSeleccionada == 1) {
                 if (plantaSeleccionada == 1) {
                     plantaCarnivora1.mostrarInfo();
@@ -115,6 +135,8 @@ int main() {
                 }
                 cout << "Ingrese la cantidad que desea pedir: ";
                 cin >> cantidad;
+
+                // Se calcula el precio total
                 double precioTotal = 0.0;
                 if (plantaSeleccionada == 1) {
                     precioTotal = plantaCarnivora1.getPrecio() * cantidad;
@@ -136,6 +158,7 @@ int main() {
                 return 0;
             }
         }
+    // Opción si se elige planta cactus:
     else if (tipoPlanta == 2) {
         cout << "\nHa seleccionado la planta cactus.\n"
         << "¿Qué planta deseas ver?\n"
@@ -144,6 +167,8 @@ int main() {
         << "3. " << plantaCactus3.getNombre() << "\n";
         int plantaSeleccionada;
         cin >> plantaSeleccionada;
+        
+        // Se muestra la planta elegida
         cout << "La planta seleccionada fue: \n";
         if (plantaSeleccionada == 1) {
             cout << plantaCactus1.getNombre() << endl;
@@ -159,12 +184,15 @@ int main() {
             return 0;
         }
 
+            // Se le pregunta al usuario que desea hacer siguiente
             cout << "¿Qué información desea ver?\n"
                  << "1. Información de la planta\n"
                  << "2. Cuidados de la planta\n"
                  << "3. Hacer un pedido\n";
             int infoSeleccionada;
             cin >> infoSeleccionada;
+
+            // Se muestra la información, precios, o cuidados correspondientes
             if (infoSeleccionada == 1) {
                 if (plantaSeleccionada == 1) {
                     plantaCactus1.mostrarInfo();
@@ -213,6 +241,8 @@ int main() {
                 }
                 cout << "Ingrese la cantidad que desea pedir: ";
                 cin >> cantidad;
+                
+                // Se calcula el precio total
                 double precioTotal = 0.0;
                 if (plantaSeleccionada == 1) {
                     precioTotal = plantaCactus1.getPrecio() * cantidad;
@@ -234,6 +264,7 @@ int main() {
                 return 0;
             }
         }
+    // Opción si se elige planta flor:
     else if (tipoPlanta == 3) {
         cout << "\nHa seleccionado la planta flor.\n"
         << "¿Qué planta deseas ver?\n"
@@ -243,6 +274,8 @@ int main() {
         << "4. " << plantaFlor4.getNombre() << "\n";
         int plantaSeleccionada;
         cin >> plantaSeleccionada;
+
+        // Se muestra la planta elegida
         cout << "La planta seleccionada fue: \n";
         if (plantaSeleccionada == 1) {
             cout << plantaFlor1.getNombre() << endl;
@@ -261,12 +294,15 @@ int main() {
             return 0;
         }
 
+            // Se le pregunta al usuario que desea hacer siguiente
             cout << "¿Qué información desea ver?\n"
                  << "1. Información de la planta\n"
                  << "2. Cuidados de la planta\n"
                  << "3. Hacer un pedido\n";
             int infoSeleccionada;
             cin >> infoSeleccionada;
+
+            // Se muestra la información, precios, o cuidados correspondientes
             if (infoSeleccionada == 1) {
                 if (plantaSeleccionada == 1) {
                     plantaFlor1.mostrarInfo();
@@ -324,6 +360,8 @@ int main() {
                 }
                 cout << "Ingrese la cantidad que desea pedir: ";
                 cin >> cantidad;
+                
+                // Se calcula el precio total
                 double precioTotal = 0.0;
                 if (plantaSeleccionada == 1) {
                     precioTotal = plantaFlor1.getPrecio() * cantidad;
