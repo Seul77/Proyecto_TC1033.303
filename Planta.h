8b4bb3580de25ee34d608b15b0941b530740c3dd
Seul77 <a01714080@tec.mx>
2025-12-07 17:49:28 -0600

    Update Planta.h

--- Planta.h
+++ Planta.h
@@ -1,17 +1,18 @@
-#ifndef PLANTA_H
+#ifndef PLANTA_H      
 #define PLANTA_H
 
-#include "PlantaInfo.h"
-#include "PlantaCuidados.h"
+#include "PlantaInfo.h"       
+#include "PlantaCuidados.h"   
 
+// Clase Planta: ESTA CLASE ES LA COMPOSICION ENTRE: PlantaInfo, PlantaCuidados
 class Planta {
 private:
-    PlantaInfo info;
-    PlantaCuidados cuidados;
+    PlantaInfo info;          // Atributo que contiene la información general
+    PlantaCuidados cuidados;  // Atributo que contiene la información sobre los cuidados
 
 public:
+    // Constructor que recibe los dos objetos como parámetros
     Planta(PlantaInfo i, PlantaCuidados c) : info(i), cuidados(c) {}
 };
 
-#endif
-
+#endif   
