98205614f8f6b3801114e5a63f435438b57b4957
Seul77 <a01714080@tec.mx>
2025-12-07 17:27:24 -0600

    Add files via upload

---
+++ Planta.h
@@ -0,0 +1,17 @@
+#ifndef PLANTA_H
+#define PLANTA_H
+
+#include "PlantaInfo.h"
+#include "PlantaCuidados.h"
+
+class Planta
+{
+private:
+    PlantaInfo info;
+    PlantaCuidados cuidados;
+
+public:
+    Planta(PlantaInfo i, PlantaCuidados c) : info(i), cuidados(c) {}
+};
+
+#endif
